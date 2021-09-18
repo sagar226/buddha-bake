7211dd61143e97601e7cdb0eb6744036f0ba4415
sagar chauhan <sagarprajapati226@gmail.com>
2021-09-18 11:31:53 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -118,3 +118,49 @@ $(function() {
   });
 
 });
+$(function() {
+
+  var inWrap = $('.inner-wrapper'),
+  $slide = $('.slide');
+
+
+  function slideNext() {
+
+    inWrap.animate({left: '-200%'}, 200, function() {
+  
+      inWrap.css('left', '-100%');
+  
+      $('.slide').last().after($('.slide').first());
+  
+    });
+  
+  }
+
+
+   //Enabling auto scroll
+   sliderInterval = setInterval(slideNext, 4000);
+
+
+
+  $('.prev').on('click', function() {
+
+    inWrap.animate({left: '0%'}, 200, function() {
+
+      inWrap.css('left', '-100%');
+
+      $('.slide').first().before($('.slide').last());
+
+    });
+  });
+
+
+  $('.next').on('click', function() {
+
+    clearInterval(sliderInterval);
+
+    slideNext();
+
+  });
+
+
+});
